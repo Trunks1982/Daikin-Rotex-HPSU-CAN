c9bc1c3fb17cae45e4bec78f23773e3b2ad8fc92
wrfz <77174685+wrfz@users.noreply.github.com>
2024-09-08 22:07:42 +0200

    Move Utils code to cpp

--- components/daikin_rotex_can/utils.cpp
+++ components/daikin_rotex_can/utils.cpp
@@ -1,9 +1,114 @@
 #include "esphome/components/daikin_rotex_can/utils.h"
+#include <regex>
 
 namespace esphome {
 namespace daikin_rotex_can {
 
 std::string Utils::g_log_filter = "";
+
+bool Utils::find(std::string const& haystack, std::string const& needle) {
+    auto it = std::search(
+        haystack.begin(), haystack.end(),
+        needle.begin(), needle.end(),
+        [](unsigned char ch1, unsigned char ch2) { return std::toupper(ch1) == std::toupper(ch2); }
+    );
+    return (it != haystack.end());
+}
+
+std::vector<std::string> Utils::split(std::string const& str) {
+    std::string segment;
+    std::istringstream iss(str);
+    std::vector<std::string> result;
+
+    while (std::getline(iss, segment, '|'))
+    {
+        if (!segment.empty()) {
+            result.push_back(segment);
+        }
+    }
+
+    return result;
+}
+
+bool Utils::is_number(const std::string& str) {
+    return !str.empty() && std::find_if(
+        str.begin(),
+        str.end(),
+        [](unsigned char chr) { return !std::isdigit(chr); }
+    ) == str.end();
+}
+
+std::string Utils::to_hex(uint32_t value) {
+    char hex_string[20];
+    sprintf(hex_string, "0x%02X", value);
+    return std::string(hex_string);
+}
+
+std::vector<uint8_t> Utils::str_to_bytes(const std::string& str) {
+    std::vector<uint8_t> bytes;
+    std::stringstream ss(str);
+    std::string byteStr;
+
+    while (ss >> byteStr) {
+        const uint8_t byte = static_cast<uint8_t>(std::stoi(byteStr, nullptr, 16));
+        bytes.push_back(byte);
+    }
+
+    return bytes;
+}
+
+std::array<uint8_t, 7> Utils::str_to_bytes_array8(const std::string& str) {
+    std::array<uint8_t, 7> byte_array{};
+
+    std::string cleaned_str = std::regex_replace(str, std::regex("[^0-9A-Fa-f\\s]+"), "");
+    cleaned_str = std::regex_replace(cleaned_str, std::regex("\\s+"), " ");
+
+    std::stringstream ss(cleaned_str);
+    std::string byte_str;
+    size_t index = 0;
+
+    while (ss >> byte_str && index < byte_array.size()) {
+        byte_array[index++] = static_cast<uint8_t>(std::stoul(byte_str, nullptr, 16));
+    }
+
+    return byte_array;
+}
+
+std::array<uint16_t, 7> Utils::str_to_bytes_array16(const std::string& str) {
+    std::array<uint16_t, 7> words;
+    std::stringstream ss(str);
+    std::string wordStr;
+    const uint16_t DC = 0xFFFF;
+
+    uint8_t index = 0;
+    while (ss >> wordStr && index < 7) {
+        const uint16_t word = (wordStr == "__") ? DC : static_cast<uint16_t>(std::stoi(wordStr, nullptr, 16));
+        words[index++] = word;
+    }
+
+    return words;
+}
+
+std::map<uint8_t, std::string> Utils::str_to_map(const std::string& input) {
+    std::map<uint8_t, std::string> result;
+    std::stringstream ss(input);
+    std::string pair;
+
+    while (std::getline(ss, pair, '|')) {
+        size_t pos = pair.find(':');
+        if (pos != std::string::npos) {
+            std::string keyStr = pair.substr(0, pos);
+            std::string value = pair.substr(pos + 1);
+
+            uint8_t key = static_cast<uint8_t>(std::strtoul(keyStr.c_str(), nullptr, 16));
+
+            result[key] = value;
+        }
+    }
+
+    return result;
+}
+
 
 }
 }
--- components/daikin_rotex_can/utils.h
+++ components/daikin_rotex_can/utils.h
@@ -4,8 +4,8 @@
 #include <algorithm>
 #include <sstream>
 #include <iomanip>
-#include <vector>
 #include <variant>
+#include <vector>
 #include <memory>
 #include <map>
 
@@ -22,44 +22,6 @@ public:
         std::snprintf(buffer.get(), size, format.c_str(), args...);
 
         return std::string(buffer.get(), buffer.get() + size - 1);
-    }
-
-    static bool find(std::string const& haystack, std::string const& needle) {
-        auto it = std::search(
-            haystack.begin(), haystack.end(),
-            needle.begin(), needle.end(),
-            [](unsigned char ch1, unsigned char ch2) { return std::toupper(ch1) == std::toupper(ch2); }
-        );
-        return (it != haystack.end());
-    }
-
-    static std::vector<std::string> split(std::string const& str) {
-        std::string segment;
-        std::istringstream iss(str);
-        std::vector<std::string> result;
-
-        while (std::getline(iss, segment, '|'))
-        {
-            if (!segment.empty()) {
-                result.push_back(segment);
-            }
-        }
-
-        return result;
-    }
-
-    static bool is_number(const std::string& str) {
-        return !str.empty() && std::find_if(
-            str.begin(),
-            str.end(),
-            [](unsigned char chr) { return !std::isdigit(chr); }
-        ) == str.end();
-    }
-
-    static std::string to_hex(uint32_t value) {
-        char hex_string[20];
-        sprintf(hex_string, "0x%02X", value);
-        return std::string(hex_string);
     }
 
     template<typename T>
@@ -82,67 +44,14 @@ public:
         return str.str();
     }
 
-    static std::vector<uint8_t> str_to_bytes(const std::string& str) {
-        std::vector<uint8_t> bytes;
-        std::stringstream ss(str);
-        std::string byteStr;
-
-        while (ss >> byteStr) {
-            const uint8_t byte = static_cast<uint8_t>(std::stoi(byteStr, nullptr, 16));
-            bytes.push_back(byte);
-        }
-
-        return bytes;
-    }
-
-    static std::array<uint8_t, 7> str_to_bytes_array8(const std::string& str) {
-        std::array<uint8_t, 7> bytes;
-        std::stringstream ss(str);
-        std::string byteStr;
-
-        uint8_t index = 0;
-        while (ss >> byteStr && index < 7) {
-            const uint8_t byte = static_cast<uint8_t>(std::stoi(byteStr, nullptr, 16));
-            bytes[index++] = byte;
-        }
-
-        return bytes;
-    }
-
-    static std::array<uint16_t, 7> str_to_bytes_array16(const std::string& str) {
-        std::array<uint16_t, 7> words;
-        std::stringstream ss(str);
-        std::string wordStr;
-        const uint16_t DC = 0xFFFF;
-
-        uint8_t index = 0;
-        while (ss >> wordStr && index < 7) {
-            const uint16_t word = (wordStr == "__") ? DC : static_cast<uint16_t>(std::stoi(wordStr, nullptr, 16));
-            words[index++] = word;
-        }
-
-        return words;
-    }
-
-    static std::map<uint8_t, std::string> str_to_map(const std::string& input) {
-        std::map<uint8_t, std::string> result;
-        std::stringstream ss(input);
-        std::string pair;
-
-        while (std::getline(ss, pair, '|')) {
-            size_t pos = pair.find(':');
-            if (pos != std::string::npos) {
-                std::string keyStr = pair.substr(0, pos);
-                std::string value = pair.substr(pos + 1);
-
-                uint8_t key = static_cast<uint8_t>(std::strtoul(keyStr.c_str(), nullptr, 16));
-
-                result[key] = value;
-            }
-        }
-
-        return result;
-    }
+    static bool find(std::string const& haystack, std::string const& needle);
+    static std::vector<std::string> split(std::string const& str);
+    static bool is_number(const std::string& str);
+    static std::string to_hex(uint32_t value);
+    static std::vector<uint8_t> str_to_bytes(const std::string& str);
+    static std::array<uint8_t, 7> str_to_bytes_array8(const std::string& str);
+    static std::array<uint16_t, 7> str_to_bytes_array16(const std::string& str);
+    static std::map<uint8_t, std::string> str_to_map(const std::string& input);
 
     template<typename... Args>
     static void log(std::string const& tag, std::string const& str_format, Args... args) {
