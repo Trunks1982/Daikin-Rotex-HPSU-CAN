7bac662c6fa2b68b6dd9b25e74ec262a77c9e5d3
Columbo <Christoph.Freundt@googlemail.com>
2024-11-11 05:09:05 +0000

    patching in bash script

---
+++ .github/workflows/src/patch_full_yaml.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "path of bash script: `pwd`"
+cp .github/workflows/resources/external_components_dev.yaml .
+sed -i 's/!/?/g' full.yaml # make yaml pyyaml conform
+cels patch full.yaml external_components_dev.yaml > full_patched.yaml
+cat full_patched.yaml > full.yaml
+sed -i 's/?/!/g' full.yaml # make yaml esphome conform
